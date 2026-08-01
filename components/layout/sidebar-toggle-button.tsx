cd1cbf72463400aeaa8c738b1dd8fcc9a4da3af3
Sainigurnoor511 <sainigurnoor511@gmail.com>
2026-08-01 22:44:04 +0530

    style: small sidebar nav item gap, smaller toggle icon
    SidebarMenu gap-0 -> gap-0.5 (2px between nav rows, was touching).
    Toggle button icons size-5 -> size-4.

--- components/layout/sidebar-toggle-button.tsx
+++ components/layout/sidebar-toggle-button.tsx
@@ -5,7 +5,7 @@ import { useSidebar } from '@/components/ui/sidebar'
 
 function OpenIcon() {
   return (
-    <svg width="20px" height="20px" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" className="size-5">
+    <svg width="20px" height="20px" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" className="size-4">
       <rect x="7" y="6.5" width="7" height="1.5" rx="0.75" transform="rotate(90 7 6.5)" fill="currentColor" />
       <rect x="3" y="4" width="14" height="12" rx="2.8" stroke="currentColor" strokeWidth="1.5" />
     </svg>
@@ -14,7 +14,7 @@ function OpenIcon() {
 
 function CollapsedIcon() {
   return (
-    <svg width="20px" height="20px" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" className="size-5">
+    <svg width="20px" height="20px" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" className="size-4">
       <rect x="10.5" y="6.5" width="7" height="5" rx="1" transform="rotate(90 10.5 6.5)" fill="currentColor" />
       <rect x="3" y="4" width="14" height="12" rx="2.8" stroke="currentColor" strokeWidth="1.5" />
     </svg>
